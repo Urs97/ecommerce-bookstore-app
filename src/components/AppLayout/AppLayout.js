e9621c0e71243d50e47017a262e42b928dd407cb
Urs97 <64044473+Urs97@users.noreply.github.com>
2020-05-12 07:02:50 +0200

    added footer

--- src/components/AppLayout/AppLayout.js
+++ src/components/AppLayout/AppLayout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './AppLayout.scss';
 import Navigation from '../Navigation/Navigation';
+import Footer from '../Footer/Footer';
 
 function AppLayout(props) {
     return (
         <React.Fragment>
             <Navigation />
             {props.children}
+            <Footer />
         </React.Fragment>
     )
 };
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.scss';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+    return (
+        <section className="footer">
+            <section className="footer-nav">
+                <Link to="/">Home</Link>
+                <Link to="/shop">Shop</Link>
+                <Link to="/about">About Us</Link>
+                <Link to="/about">TOS</Link>
+                <Link to="/about">Contact Us</Link>
+            </section>
+            <p>ThemeREX © 2020 All rights reserved</p>
+        </section>
+    )
+}
+
+export default Footer;

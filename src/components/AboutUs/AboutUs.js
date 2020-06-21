804b15c14cfa5c3750cd14a0fcf2612a0bc21655
Urs97 <64044473+Urs97@users.noreply.github.com>
2020-06-22 00:21:22 +0200

    started adding content to about us page

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -31,6 +31,17 @@ function AboutUs() {
     return (
         <main>
             <PageHeader title="About Us" color="yellow"/>
+            <section className="about-us-main">
+                <h1 className="title">Our Mission</h1>
+                <p>To provide the absolute best possible ecommerce bookstore experience.</p>
+                <h2 className="title">About</h2>
+                <p>Welcome to Bookz! We aim to offer our customers a variety of the digitally available books. We’ve come a long way, so we know exactly which direction to take when supplying you with high quality yet budget-friendly products. We offer all of this while providing excellent customer service and friendly support. We always keep an eye on the latest trends in ecommerce and put our customers’ wishes first. That is why we have satisfied customers all over the world, and are thrilled to be a part of the ecommerce industry. The interests of our customers are always top priority for us, so we hope you will enjoy our products as much as we enjoy making them available to you.</p>
+                <h2 className="title">Leadership</h2>
+                <section className="leadership">
+                    <div>Co founder 1 template</div>
+                    <div>Co founder 2 template</div>
+                </section>
+            </section>
             <BulletPointContainer mode={"expanded"} icons={icons} titles={titles} texts={texts} color={"yellow"}/>
         </main>
     )
